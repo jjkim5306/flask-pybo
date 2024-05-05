699fbe83875615902b38ae795ab1da3d189f45c1
Jeff <jjkim5306@gmail.com>
2024-05-05 23:11:48 +0900

    파이보 프로젝트 최초 커밋

---
+++ myproject.cmd
@@ -0,0 +1,5 @@
+@echo off
+cd C:\Users\jjkim\AppData\Local\Programs\Python\Python312\venvs\myproject
+set FLASK_APP=pybo
+set FLASK_DEBUG=true
+C:\Users\jjkim\AppData\Local\Programs\Python\Python312\venvs\myproject\Scripts\activate
